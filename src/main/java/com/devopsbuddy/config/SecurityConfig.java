44d4d9ceab93a36b954f25b5b88b5f70eabe87a0
fetahokey <ouldkradda.fateh@gmail.com>
2018-10-11 13:17:14 +0100

    Security update

---
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.devopsbuddy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+* <h1>devopsbuddy!</h1>
+* The devopsbuddy implements an application that
+* simply displays "Hello World!" to the standard output.
+* <p>
+* Giving proper comments in your program makes it more
+* user friendly and it is assumed as a high quality code.
+*
+*
+* @author  OULD KRADDA FATEH
+* @version 1.0.0
+* @since   2018-10-10
+*       TO 10/10/2018 3:16 PM
+*/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    /**
+     * Public URLs. *
+     * **/
+    private static final String[] PUBLIC_MATCHERS = {
+        "/webjars/**",
+        "/css/**",
+        "/js/**",
+        "/images/**",
+        "/",
+        "/about/**",
+        "/contact/**",
+        "/error/**/**",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+                http
+                .authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+                auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER");
+    }
+}
