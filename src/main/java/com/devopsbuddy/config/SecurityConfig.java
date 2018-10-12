73485ec72f68736eb411ea2cf00835fe198d7b1f
fetahokey <ouldkradda.fateh@gmail.com>
2018-10-12 20:06:57 +0100

    Enabled Spring security for the web site

--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -6,8 +6,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
 * <h1>devopsbuddy!</h1>
@@ -46,7 +53,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .formLogin().loginPage("/login")
+                        .successHandler(
+                                new AuthenticationSuccessHandler(){
+                                    @Override
+                                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                                        System.out.println("Authentication successful ! ");
+                                    }
+                                })
+                        .defaultSuccessUrl("/payload")
                 .failureUrl("/login?error").permitAll()
                 .and()
                 .logout().permitAll();
@@ -57,7 +72,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 auth
                 .inMemoryAuthentication()
                 .withUser("user")
-                .password("password")
+                .password("{noop}password")
                 .roles("USER");
     }
 }
---
+++ src/main/java/com/devopsbuddy/config/TemplateEngineConfig.java
@@ -0,0 +1,40 @@
+package com.devopsbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class TemplateEngineConfig {
+
+
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
+
+    public ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf template engine with Spring integration")
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.addDialect(securityDialect());
+        return templateEngine;
+    }
+
+
+}
